22b93d45b92be16e17a3897195465b14d89cc4f0
Cheng Qi <30358050+Krischengwill@users.noreply.github.com>
2018-12-09 20:16:11 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8000;
+
 
 app.use(express.static(__dirname + '/index.html'));
-app.listen(port, function() {
-    console.log('App is running on port ' + port);
- });
+app.listen(process.env.PORT || 8000);
